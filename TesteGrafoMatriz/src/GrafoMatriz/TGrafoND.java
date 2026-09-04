19202179fd44e1fa1faf09e38db6e5206cbfb637
PedroCJunior <pedro.casas.pequeno@gmail.com>
2026-09-04 13:42:11 +0000

    Ex 8, pulamos o 7 por enquanto

---
+++ TesteGrafoMatriz/src/GrafoMatriz/TGrafoND.java
@@ -0,0 +1,55 @@
+package GrafoMatriz;
+
+public class TGrafoND {
+	// Atributos Privados
+	private	int n; // quantidade de vértices
+	private	int m; // quantidade de arestas
+	private	int adj[][]; //matriz de adjacência
+	// Métodos Públicos
+	public TGrafoND( int n) {  // construtor
+	    this.n = n;
+	    // No início dos tempos não há arestas
+	    this.m = 0; 
+	    // alocação da matriz do TGrafo
+	    this.adj = new int [n][n];
+
+	    // Inicia a matriz com zeros
+		for(int i = 0; i< n; i++)
+			for(int j = 0; j< n; j++)
+				this.adj[i][j]=0;	
+	}
+
+	// Insere uma aresta no Grafo Não Dirigido tal que
+	// v é adjacente a w
+	public void insereAND(int v, int w) {
+	    // testa se nao temos a aresta
+	    if(adj[v][w] == 0 ){
+	        adj[v][w] = 1;
+			adj[w][v] = 1;
+	        m++; // atualiza qtd arestas
+	    }
+	}
+	
+	// remove uma aresta v->w do Grafo Não Dirigido
+	public void removeAND(int v, int w) {
+	    // testa se temos a aresta
+	    if(adj[v][w] == 1 ){
+	        adj[v][w] = 0;
+			adj[w][v] = 0;
+	        m--; // atualiza qtd arestas
+	    }
+	}
+
+		public void showND() {
+	    System.out.println("n: " + n );
+	    System.out.println("m: " + m );
+	    for( int i=0; i < n; i++){
+	    	System.out.print("\n");
+	        for( int w=0; w < n; w++)
+	            if(adj[i][w] == 1)
+	            	System.out.print("Adj[" + i + "," + w + "]= 1" + " ");
+	            else System.out.print("Adj[" + i + "," + w + "]= 0" + " ");
+	    }
+	    System.out.println("\nEnfim da impressao do grafo." );
+	}
+}
--- TesteGrafoMatriz/src/GrafoMatriz/TesteGrafoMatriz.java
+++ TesteGrafoMatriz/src/GrafoMatriz/TesteGrafoMatriz.java
@@ -3,21 +3,32 @@ package GrafoMatriz;
 public class TesteGrafoMatriz {
 	public static void main(String args[]) {
 		//  chama o construtor para criar um grafo 4x4
-		TGrafo g = new TGrafo(4);
+		//TGrafo g = new TGrafo(4);
+		TGrafoND g = new TGrafoND(4);
 		//insere as arestas do grafo
 		//A={(0,1),(0,2),(2,1),(2,3),(1,3)}
-		g.insereA(0 , 1);
-		g.insereA(0,2);
-		g.insereA(2,1);
-		g.insereA(2,3);
-		g.insereA(1,3);
-		g.inDegree(3);
-		g.outDegree(3);
-		g.degree(3);
-		g.fonte(3);
-		g.sorvedouro(3);
-		g.ehSimetrico(g);
+		// g.insereA(0 , 1);
+		// g.insereA(0,2);
+		// g.insereA(2,1);
+		// g.insereA(2,3);
+		// g.insereA(1,3);
+		// g.inDegree(3);
+		// g.outDegree(3);
+		// g.degree(3);
+		// g.fonte(3);
+		// g.sorvedouro(3);
+		// g.ehSimetrico(g);
+
+		g.insereAND(0 , 1);
+		g.insereAND(0,2);
+		g.insereAND(2,1);
+		g.insereAND(2,3);
+		g.insereAND(1,3);
+		g.showND();
+
+		g.removeAND(0,1);
+		g.showND();
 		// mostra o grafo preenchido
-		g.show();
+		//g.show();
 	}
 }
